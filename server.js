4874446a44a8c3282aef3cf07b0ccdff1d2d602d
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-12 15:45:11 +0100

    add route bacheca, add first content and images folder

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
@@ -9,3 +11,20 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send('Server del mio blog');
 });
+
+app.get('/bacheca', (req, res) => {
+
+    const bacheca = [
+
+    {
+        titolo: 'Ciambellone',
+        contenuto: 'lorem ipsum',
+        img:'/images/ciambellone.jpeg',
+        tags: ['tag1','tag2','tag3']
+    }
+
+    ]
+
+    res.json(bacheca)
+
+})
