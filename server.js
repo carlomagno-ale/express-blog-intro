f61f2853e1f3b13be7277ab6210318c7fb2c459a
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-12 16:00:32 +0100

    add array with 5 post

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('express-blog-intro'));
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
@@ -21,10 +21,35 @@ app.get('/bacheca', (req, res) => {
         contenuto: 'lorem ipsum',
         img:'/images/ciambellone.jpeg',
         tags: ['tag1','tag2','tag3']
-    }
+    },
+    {
+        titolo: 'Cracker alla barbabietola',
+        contenuto: 'lorem ipsum',
+        img:'/images/cracker_barbabietola.jpeg',
+        tags: ['tag1','tag2','tag3']
+    },
+    {
+        titolo: 'Pane fritto dolce',
+        contenuto: 'lorem ipsum',
+        img:'/images/pane_fritto_dolce.jpeg',
+        tags: ['tag1','tag2','tag3']
+    },
+    {
+        titolo: 'Pasta_barbabietola.jpeg',
+        contenuto: 'lorem ipsum',
+        img:'/images/pasta_barbabietola.jpeg',
+        tags: ['tag1','tag2','tag3']
+    },
+    {
+        titolo: 'Torta_paesana.jpeg',
+        contenuto: 'lorem ipsum',
+        img:'/images/torta_paesana.jpeg',
+        tags: ['tag1','tag2','tag3']
+    },
 
     ]
 
     res.json(bacheca)
 
 })
+
