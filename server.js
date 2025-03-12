e60e4016d87279add85f11ebbebe255dd0bf2446
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-12 16:04:15 +0100

    edit path for static assets

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-app.use(express.static('express-blog-intro'));
+app.use(express.static('images'));
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
@@ -19,31 +19,31 @@ app.get('/bacheca', (req, res) => {
     {
         titolo: 'Ciambellone',
         contenuto: 'lorem ipsum',
-        img:'/images/ciambellone.jpeg',
+        img:'/ciambellone.jpeg',
         tags: ['tag1','tag2','tag3']
     },
     {
         titolo: 'Cracker alla barbabietola',
         contenuto: 'lorem ipsum',
-        img:'/images/cracker_barbabietola.jpeg',
+        img:'/cracker_barbabietola.jpeg',
         tags: ['tag1','tag2','tag3']
     },
     {
         titolo: 'Pane fritto dolce',
         contenuto: 'lorem ipsum',
-        img:'/images/pane_fritto_dolce.jpeg',
+        img:'/pane_fritto_dolce.jpeg',
         tags: ['tag1','tag2','tag3']
     },
     {
         titolo: 'Pasta_barbabietola.jpeg',
         contenuto: 'lorem ipsum',
-        img:'/images/pasta_barbabietola.jpeg',
+        img:'/pasta_barbabietola.jpeg',
         tags: ['tag1','tag2','tag3']
     },
     {
         titolo: 'Torta_paesana.jpeg',
         contenuto: 'lorem ipsum',
-        img:'/images/torta_paesana.jpeg',
+        img:'/torta_paesana.jpeg',
         tags: ['tag1','tag2','tag3']
     },
 
